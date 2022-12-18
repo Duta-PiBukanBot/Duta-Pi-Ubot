06243e41b6b760bc78047773f7aa969249d70b55
Duta-PiBukanBot <109447804+Duta-PiBukanBot@users.noreply.github.com>
2022-12-18 12:32:18 +0700

    Update hide.py

--- tod/assistant/hide.py
+++ tod/assistant/hide.py
@@ -10,12 +10,12 @@ from tod import tod
 @tod.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./tod/hide.txt"):
-        jsondata = json.load(open("./tod/hide.txt"))
+    if os.path.exists("./Duta-Pi-Ubot/hide.txt"):
+        jsondata = json.load(open("./Duta-Pi-Ubot/hide.txt"))
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
-            reply_pop_up_alert = "This message no longer exists in tod server"
+            reply_pop_up_alert = "This message no longer exists in Duta-Pi-Ubot server"
     else:
         reply_pop_up_alert = "This message no longer exists "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
